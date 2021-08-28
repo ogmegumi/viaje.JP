a1139ad0e3285ac5f32803ef4e6b2b52dd39d8f6
ogmegumi <ogmegumi@gmail.com>
2021-08-28 06:53:07 +0000

    fixed migrate for tags_relationships

--- db/migrate/20210815053007_create_tags_relationship.rb
+++ db/migrate/20210815053007_create_tags_relationship.rb
@@ -2,8 +2,6 @@ class CreateTagsRelationship < ActiveRecord::Migration[5.2]
   def change
     create_table :tags_relationships do |t|
       t.timestamps null: false
-      t.bigint :post, null: false
-      t.bigint :tag, null: false
     end
     add_reference :tags_relationships, :post, foreign_key: true
     add_reference :tags_relationships, :tag, foreign_key: true
